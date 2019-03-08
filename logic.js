4ada8cafe1c6c526c699aa9942639bdadaa620eb
1curtislee <1curtislee@gmail.com>
2019-03-08 15:29:53 -0800

    logic working, layout split, add button working

---
+++ logic.js
@@ -0,0 +1,53 @@
+var topics = [
+  "music",
+  "jazz",
+  "rock n' roll",
+  "guitar",
+  "sax",
+  "drums",
+  "beats",
+  "sing",
+]
+
+function setup() {
+  for (x=0; x<topics.length; x++) {
+    renderButton(topics[x]);
+  }
+}
+
+//button render function 
+function renderButton(x) {
+  var gifButton = $('<button>')
+    gifButton.attr('data-topic', x);
+    gifButton.addClass("btn btn-success");
+    gifButton.text(x);
+    $("#buttons").append(gifButton);
+}
+
+//ajax request & print gifs to document
+$(document.body).on("click", ".btn", function() {
+  var searchTerm = $(this).attr("data-topic");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=nrylwrZG52SmIojDMTKvClp4ZWGL6osm&q=" + searchTerm + "&limit=10&offset=0&rating=G&lang=en"
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function(response) {
+      var gifs = ''
+      for (x=0; x<response.data.length; x++) {
+        var gifResponse = response.data[x].images.original.url;
+        gifs += '<img src="' + gifResponse + '">';
+      }
+      $("#gifs").html(gifs);
+    });
+});
+
+//add button:
+$("#submit").on('click', function() {
+  var newButton = $("#userInput").val();
+  renderButton(newButton)
+});
+
+
+
+
